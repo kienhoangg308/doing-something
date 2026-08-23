fad6f5866df0271074c491328db0950c3a0c4959
kienhoang <kienhoangg308@gmail.com>
2026-08-23 22:41:52 +0700

    add new commit to random1

--- demo.java
+++ demo.java
@@ -3,11 +3,10 @@ hello
 feaA
 feaB
 feaB1
-feaB2
-feaB3
-feaB4
-feaB5
-                new function
-another sunrise
-        newmessage
-create account
+//feaB2
+//feaB3
+//feaB4
+//feaB5
+//                new function
+//another sunrise
+//create account
